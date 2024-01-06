bd7626072a1a61894ab91782f2b29cf87fb70972
Keith Brooks <keith1225@outlook.com>
2024-01-05 19:07:46 -0500

    add live reloading

--- src/utils/hydrate.tsx
+++ src/utils/hydrate.tsx
@@ -1,4 +1,18 @@
-import { hydrate, ComponentType } from 'preact'
+import { hydrate, render, ComponentType } from 'preact'
+
+declare global {
+  interface Window {
+    LIVE_RELOAD?: boolean
+  }
+}
+
+const LIVE_RELOAD = window.LIVE_RELOAD ?? false
+
+if (LIVE_RELOAD) {
+  new EventSource('/esbuild').addEventListener('change', () =>
+    location.reload()
+  )
+}
 
 export default function clientHydrate<T>(
   Component: ComponentType<T>,
@@ -6,8 +20,12 @@ export default function clientHydrate<T>(
 ) {
   if ('location' in globalThis) {
     const propsElement = document.getElementById(propsId)
-    const props = JSON.parse(propsElement!.textContent ?? '{}')
-    hydrate(
+    const props = LIVE_RELOAD
+      ? {}
+      : JSON.parse(propsElement!.textContent ?? '{}')
+
+    const renderFn = LIVE_RELOAD ? render : hydrate
+    renderFn(
       <Component {...props} />,
       document.getElementById('root') as HTMLElement
     )
